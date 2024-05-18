e4f1785ff99c1a74368c5713bb91dd86068b16b7
Greenlamp <gabriel.knuts@gmail.com>
2024-05-18 21:57:38 +0200

    the least i can for now

--- src/test/walker.test.ts
+++ src/test/walker.test.ts
@@ -1,24 +1,10 @@
 import {beforeAll, describe, expect, it} from "vitest";
 import {initStatsKeys} from "#app/ui/game-stats-ui-handler";
-import {initPokemonPrevolutions} from "#app/data/pokemon-evolutions";
-import {initBiomes} from "#app/data/biomes";
-import {initEggMoves} from "#app/data/egg-moves";
-import {initPokemonForms} from "#app/data/pokemon-forms";
-import {initMoves} from "#app/data/move";
-import {initAbilities} from "#app/data/ability";
-import {initSpecies} from "#app/data/pokemon-species";
 import {MoneyAchv} from "#app/system/achv";
 
 describe("big tests", () => {
     beforeAll(async () => {
         initStatsKeys();
-        initPokemonPrevolutions();
-        initBiomes();
-        initEggMoves();
-        initPokemonForms();
-        initSpecies();
-        initMoves();
-        initAbilities();
     })
 
     it ('should check achv', () => {
