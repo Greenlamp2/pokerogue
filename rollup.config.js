79b86b7c70b4c6252698ccec3c74a37bda45a42f
Greenlamp <gabriel.knuts@gmail.com>
2024-05-22 23:22:02 +0200

    working build in 17s

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import incremental from '@mprt/rollup-plugin-incremental'
+import typescript from '@rollup/plugin-typescript';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import meta from 'rollup-plugin-import-meta-env';
+import json from '@rollup/plugin-json';
+
+export default [
+  {
+    input: 'src/main.ts',
+    //ATTENTION: treeshaking must be disabled!
+    // treeshake: false,
+    output: [
+      {
+        name: 'Pokerogue',
+        format: 'umd',
+        file: 'dist/pokerogue.umd.js',
+        globals: { phaser: 'Phaser' },
+        //ATTENTION: preserveModules must be enabled!
+        // preserveModules: true,
+        // preserveModulesRoot: 'src',
+        //ATTENTION: minifyInternalExports must be disabled!
+        minifyInternalExports: false
+      },
+    ],
+    external: ['phaser'],
+    plugins: [
+      // incremental(),
+      typescript({ module: "ES2020" }),
+      json(),
+      // resolve(),
+      //ATTENTION: this fixes issues with syntheticNamedExports in commonjs modules
+      //it should be last
+      // incremental.fixSNE(),
+    ]
+  }
+];
